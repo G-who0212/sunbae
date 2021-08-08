2616673388006abb1c122619feeb2cfe5a7a000c
eugene02876 <eugene02876@gmail.com>
2021-08-08 23:33:05 +0900

    develop again

--- account/forms.py
+++ account/forms.py
@@ -7,7 +7,7 @@ from .models import Career, Univ
 class RegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'password1', 'password2', 'profile_image', 'name', 'major', 'studentNum', 'campus', 'schoolEmail', 'websiteName', 'websiteURL', 'followers', 'following']
+        fields = ['username', 'password1', 'password2', 'profile_image', 'name', 'major', 'studentNum', 'campus', 'schoolEmail', 'websiteName', 'websiteURL']
         # 'career1Company', 'career1Title', 'career1YearStart', 'career1MonthStart', 'career1YearEnd', 'career1MonthEnd', 'career1RoleAchievement', 'university1Activity', 'university1Title', 'university1YearStart', 'university1MonthStart', 'university1YearEnd', 'university1MonthEnd', 'university1RoleAchievement', 'websiteName', 'websiteURL']
 
 class CareerForm(forms.ModelForm):
