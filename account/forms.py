bd0c2cdd24334ae7b6fb8b84dd064c2a87de1586
eugene02876 <eugene02876@gmail.com>
2021-08-11 00:35:49 +0900

    develop 3
    fighting~~~

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, UserCreationForm
 from .models import CustomUser
 from django import forms
 from .models import Career, Univ
@@ -9,19 +9,23 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'password1', 'password2', 'profile_image', 'name', 'major', 'studentNum', 'campus', 'schoolEmail', 'websiteName', 'websiteURL']
-        # 'career1Company', 'career1Title', 'career1YearStart', 'career1MonthStart', 'career1YearEnd', 'career1MonthEnd', 'career1RoleAchievement', 'university1Activity', 'university1Title', 'university1YearStart', 'university1MonthStart', 'university1YearEnd', 'university1MonthEnd', 'university1RoleAchievement', 'websiteName', 'websiteURL']
+        labels = {'username':'아이디','profile_image':'프로필 사진','name':'이름','major':'전공','studentNum':'학번','campus': '캠퍼스','schoolEmail':'학교 이메일','websiteName':'웹사이트 이름','websiteURL':'웹사이트 URL'}
+
 
 class CareerForm(forms.ModelForm):
     class Meta:
         model = Career
-        fields = ['company', 'title', 'careerYearStart', 'careerMonthStart', 'careerYearEnd', 'careerMonthEnd', 'careerRoleAchievement']
+        fields = ['company', 'title', 'careerYearStart', 'careerMonthStart', 'careerYearEnd', 'careerMonthEnd']
+        labels = {'company': '회사', 'title':'직함', 'careerYearStart':'시작 연도', 'careerMonthStart':'시작 월', 'careerYearEnd':'종료 연도', 'careerMonthEnd':'종료 월'}
 
 class UnivForm(forms.ModelForm):
     class Meta:
         model = Univ
-        fields = ['universityActivity', 'universityTitle', 'university_YearStart', 'university_MonthStart', 'university_YearEnd', 'university_MonthEnd', 'university_RoleAchievement']
+        fields = ['universityActivity', 'universityTitle', 'university_YearStart', 'university_MonthStart', 'university_YearEnd', 'university_MonthEnd']
+        labels = {'universityActivity': '대학생 활동', 'universityTitle':'직함', 'university_YearStart':'시작 연도', 'university_MonthStart':'시작 월', 'university_YearEnd':'종료 연도', 'university_MonthEnd':'종료 월'}
 
 class ChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ['profile_image', 'name', 'major', 'studentNum', 'campus', 'schoolEmail', 'websiteName', 'websiteURL']
+        labels = {'profile_image': '프로필 사진', 'name': '이름', 'major': '전공', 'studentNum': '학번', 'campus': '캠퍼스', 'schoolEmail': '학교 이메일', 'websiteName': '웹사이트 이름', 'websiteURL': '웹사이트 URL'}
--- post/forms.py
+++ post/forms.py
@@ -5,3 +5,4 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['category', 'body', 'hashtag', 'image']
+        labels = {'category':'카테고리', 'body': '내용', 'hashtag': '해시태그', 'image': '이미지'}
